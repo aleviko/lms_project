86f902369f4390f27aa75a6227c21edd89991448
Aleksey Kokarev <aleviko@mail.ru>
2023-10-10 12:04:34 +0400

    20231010 11:57 Урок 6 до 23:35 login и register (кроме аватара!) наладил. Замечен косяк: джанговская админка пишет в БД пароли открытым текстом и залогиниться потом невозможно.

--- lms_project/auth_app/views.py
+++ lms_project/auth_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
 from .models import User
 
 # Create your views here.
@@ -15,12 +15,12 @@ def login(request):
         уже залогиненного пользователя. Попробовать:
         if request.user.is_authenticated:
             return HttpResponse('Вы уже вошли в систему, нет смысла делать это повторно.')'''
-        user1 = authenticate(email=data['email'], password=data['password'])
+        user1 = authenticate(request, email=data['email'], password=data['password'])
         if user1 is not None and user1.is_active:  # пользователь найден (non none) и не заблокирован
-            login(request, user1)  # вход пользователя - тут ругается: login() takes 1 positional argument but 2 were given
+            dj_login(request, user1)  # вход пользователя - тут ругается: login() takes 1 positional argument but 2 were given
             return redirect('index')  # перенаправляем пользователя на главную страницу
         else:
-            if user:
+            if user1:
                 return HttpResponse('Ваша учетная запись заблокирована!')
             else:
                 return HttpResponse('Неправильное имя или пароль')
@@ -33,10 +33,10 @@ def register(request):
         data = request.POST
         user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'],
                     birthday=data['birthday'], description=data['description'],
-                    avatar=data['avatar'])  # объект класа model.User, кроме пароля
+                    avatar=data['avatar'])  # объекты класса model.User, кроме пароля
         user.set_password(data['password'])  # пароль сразу преобразуется в хэш
         user.save()  # сохранение записи пользователя
-        login(request, user)  # вход пользователя
+        dj_login(request, user)  # вход пользователя
         return redirect('index')
     else:
         return render(request, 'register.html')  # на запрос GET возвращаем пустую форму для регистрации
@@ -44,7 +44,7 @@ def register(request):
 
 
 def logout(request):
-    logout(request)
+    dj_logout(request)
     return redirect('login')
     # return HttpResponse('Это представление выполняет <b>выход</b> и редирект на страницу входа')
 
