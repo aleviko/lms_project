3f25cb8eea9d24188e5bf12e0c8b47692d9215e7
Aleksey Kokarev <aleviko@mail.ru>
2023-11-06 20:32:19 +0400

    20231106 20:30 Переделал форму создания урока в Уроке 12 на form.as_p для срабатывания стиля error_field textarea

--- lms_project/learning/forms.py
+++ lms_project/learning/forms.py
@@ -29,10 +29,10 @@ class LessonForm(forms.ModelForm):
     # А по-хорошему это поле вообще надо скрыть и заполнить автоматом.
     preview = forms.CharField(widget=Textarea(attrs={'placeholder': 'Описание содержания урока',
                               'rows': 20, 'cols': 35}),
-                              label='Описание', required=True)
+                              label='Описание', required=True)  # В видео на 16:59 required=True нет
     # выделили на экране 700 символов, а в модели 200, но тут мы переопределили, а в БД поле текстовое
     # поэтому сохранение 700 символов будет работать, если не ограничим специально (см. clean_preview)
-    error_css_class = 'error_field'  # HTML class для полей, не прошедших валидацию, в SCC прописал, но не работает - не попадает в HTML.
+    error_css_class = 'error_field'  # HTML class для полей, не прошедших валидацию для формы на {{ form.as_p }}, в SCC можно расцветить
     required_css_class = 'required_field'  # HTML class для МЕТОК обязательных полей - работает, но независимо от содержимого полей
 
     class Meta:
@@ -40,13 +40,14 @@ class LessonForm(forms.ModelForm):
         fields = ('name', 'preview', 'course')  # '__all__'
         # тут можно задать атрибуты для всех полей:
         # labels = {'name': '', 'preview': '', 'course': ''}
+        # widgets = {...
         # help_texts = {'name': 'Название урока', 'preview': 'Содержание урока'}
         # и т.п. но все равно рутины много и ненаглядно
 
     def clean_preview(self):  # переопределение метода проверки для одного поля
         preview_data = self.cleaned_data['preview']
         # извлекаем текст из проверенных данных формы (там 700 символов допустимы)
-        if len(preview_data) > 200:  # 200, но я замучался напихивать по столько
+        if len(preview_data) > 20:  # 200, но я замучался напихивать по столько
             raise ValidationError('Описание курса не должно быть длиннее 20(после отладки переделать на 200) символов')
         return preview_data
 
